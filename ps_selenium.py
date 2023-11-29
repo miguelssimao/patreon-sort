a0ecd0106db65a3aec56e2055b6b6615938abae1
Miguel <46794271+miguelssimao@users.noreply.github.com>
2023-11-29 16:44:29 +0000

    Update ps_selenium.py

--- ps_selenium.py
+++ ps_selenium.py
@@ -3,30 +3,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-def webDriver():
+def setupDriver():
+    global driver
     options = Options()
     options.add_argument("--lang=en")
     options.add_argument("--headless")
+    options.page_load_strategy = "none"
     options.add_argument("--disable-gpu")
     options.add_argument("--log-level=3")
     options.add_argument("--blink-settings=imagesEnabled=false")
-    return webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
 
 
-def findElements(selector, driver):
+def openProfile(username):
+    driver.get("https://www.patreon.com/" + username + "/posts")
+
+
+def findElements(selector):
     return driver.find_elements(By.CSS_SELECTOR, selector)
 
 
-def findComments(i, results):
-    elem = i.find_element(By.TAG_NAME, "p")
+def findComments(x, results):
+    elem = x.find_element(By.TAG_NAME, "p")
     results.append(int(elem.text))
 
 
-def findHrefs(i, results):
-    elem = i.find_element(By.TAG_NAME, "a")
+def findHrefs(x, results):
+    elem = x.find_element(By.TAG_NAME, "a")
     results.append(elem.get_attribute("href"))
 
 
-def findLikes(i, selector, results):
-    elem = i.find_element(By.CSS_SELECTOR, selector)
+def findLikes(x, selector, results):
+    elem = x.find_element(By.CSS_SELECTOR, selector)
     results.append(int(elem.text))
